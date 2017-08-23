bc0d603d499946f1811e6d36d272ee47fa67eb0e
sonofachamp <fischer.nick1@gmail.com>
2017-08-23 01:01:28 -0500

    checkpoint for diff

--- main.go
+++ main.go
@@ -26,6 +26,15 @@ var (
 	{
 		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
 	}`
+
+	fragmentShaderSource = `
+	#version 330 core
+	out vec4 FragColor;
+	
+	void main()
+	{
+		FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	} `
 )
 
 func main() {
@@ -52,26 +61,61 @@ func main() {
 		panic(err)
 	}
 
-	var vbo uint32
-	gl.GenBuffers(1, &vbo)
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BufferData(vbo, len(verticies)*4, gl.Ptr(verticies), gl.STATIC_DRAW)
-
 	vertexShader := gl.CreateShader(gl.VERTEX_SHADER)
 	cstr, free := gl.Strs(vertexShaderSource)
 	free()
 	gl.ShaderSource(vertexShader, 1, cstr, nil)
 	gl.CompileShader(vertexShader)
+	// TODO: add err checking after shader compiling
+
+	fragmentShader := gl.CreateShader(gl.FRAGMENT_SHADER)
+	cstr, free = gl.Strs(fragmentShaderSource)
+	free()
+	gl.ShaderSource(fragmentShader, 1, cstr, nil)
+	gl.CompileShader(fragmentShader)
+
+	program := gl.CreateProgram()
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+	// TODO: check for shader link errors
+
+	gl.UseProgram(program)
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, len(verticies)*4, gl.Ptr(verticies), gl.STATIC_DRAW)
+
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
+
+	gl.BindBuffer(gl.ARRAY_BUFFER, 0)
+	gl.BindVertexArray(0)
 
 	for !window.ShouldClose() {
 
 		gl.ClearColor(0.0, 1.0, 0.0, 1.0)
 		gl.Clear(gl.COLOR_BUFFER_BIT)
 
+		gl.UseProgram(program)
+		gl.BindVertexArray(vao)
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
 		window.SwapBuffers()
 		glfw.PollEvents()
 
 	}
+
+	gl.DeleteVertexArrays(1, &vao)
+	gl.DeleteBuffers(1, &vbo)
 }
 
 func keyCallback(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
